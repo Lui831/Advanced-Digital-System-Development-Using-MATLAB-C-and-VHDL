d276ac09718fba4e60d16cf3f5fafbf98eedc954
Thiagohotmail <77792298+Thiagohotmail@users.noreply.github.com>
2025-10-04 06:51:45 -0300

    Correction of moving_avg.m (incomplete)

--- 2_LINGUAGEM_C_E_RTOS/5_MATLAB_C_CODER/moving_avg.m
+++ 2_LINGUAGEM_C_E_RTOS/5_MATLAB_C_CODER/moving_avg.m
@@ -9,10 +9,14 @@ f_in = matrix_in;
 data_out=zeros(1,(length(f_in(:,2))- num_terms_inside));
 
 L= length(f_in);
-
+sum=0;
 for i = 1:(L-num_terms_inside)
-    sum = f_in(i,2) + f_in(i+1,2) + f_in(i+2,2) + f_in(i+3,2);
-    data_out(i) = sum/num_terms_inside;
+    for j =1:num_terms_inside
+
+        sum = sum + f_in(i+j-1,2);
+    end
+       data_out(i) = sum/num_terms_inside;
+       sum=0;       
 end
 
 
